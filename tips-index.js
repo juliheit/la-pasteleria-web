f81d48786b1faff644624fd8e1a304ad04787315
Sol <Sol.gayarin@gmail.com>
2025-07-28 16:26:07 -0300

    Cambios en el inicio

---
+++ tips-index.js
@@ -0,0 +1,26 @@
+const tipsPasteleria = [
+  {
+    id: 1,
+    consejo: "<b>Ingredientes a Temperatura Ambiente:</b> Para lograr mezclas homogéneas y una textura perfecta, asegurate de que ingredientes como huevos, manteca (mantequilla) y queso crema estén a temperatura ambiente.",
+  },
+  {
+    id: 2,
+    consejo: "<b>Tamizá tus Secos:</b> Tamizar la harina, el cacao y el polvo de hornear no solo elimina grumos, sino que también airea los ingredientes, resultando en bizcochuelos y preparaciones más <b>esponjosas y ligeras</b>.",
+  },
+  {
+    id: 3,
+    consejo: "<b>No te Apures con el Desmolde:</b> La paciencia es clave. Dejá enfriar bien las tortas, flanes y tartas en el molde antes de intentar desmoldarlos. Esto evita que se rompan y asegura una presentación impecable.",
+  },
+  {
+    id: 4,
+    consejo: "<b>Medí con Precisión:</b> La pastelería es química. Usá balanzas para los ingredientes secos y medidores para líquidos. Las proporciones exactas son fundamentales para el éxito de tus preparaciones.",
+  },
+  {
+    id: 5,
+    consejo: "<b>Horno Precalentado y Estable:</b> Siempre precalentá tu horno al menos 15-20 minutos antes de meter tu preparación. Una temperatura constante desde el inicio es crucial para una cocción uniforme.",
+  },
+  {
+    id: 6,
+    consejo: "<b>No Abras el Horno Constantemente:</b> Resistí la tentación de abrir el horno, especialmente durante los primeros 2/3 del tiempo de cocción. Los cambios de temperatura pueden hacer que las masas se bajen.",
+  },
+];
